5d5e51375ab6fe7d2172f45059f214b8e442d5c3
SweetOps <vladimir.fess@gmail.com>
2019-08-28 21:13:40 +0300

    feat: bump to terraform v12

--- variables.tf
+++ variables.tf
@@ -1,75 +1,77 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
+  default     = ""
   description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
+  default     = ""
   description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
+  default     = ""
   description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Set to false to prevent the module from creating any resources"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
 }
 
-variable "context" {
-  type        = "map"
-  default     = {}
-  description = "Default context to use for passing state between label invocations"
-}
-
 variable "org_id" {
+  type        = string
   default     = ""
   description = "The numeric ID of the organization this project belongs to"
 }
 
 variable "folder_id" {
+  type        = string
   default     = ""
   description = "The numeric ID of the folder this project should be created under"
 }
 
 variable "enable_deletion_protection" {
-  default     = "false"
+  type        = bool
+  default     = false
   description = "If true, the Terraform resource can be deleted without deleting the Project via the Google API."
 }
 
 variable "auto_create_network" {
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Create the 'default' network automatically"
 }
 
 variable "billing_account_id" {
-  description = "The alphanumeric ID of the billing account this project belongs to"
+  type        = string
   default     = ""
+  description = "The alphanumeric ID of the billing account this project belongs to"
 }
