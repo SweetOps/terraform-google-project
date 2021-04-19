221f40d0e9124c9d4a99443b40a51a48108422f2
Vladimir <vladimir.fess@gmail.com>
2021-04-19 17:25:53 +0300

    feat: switch to CP null-label (#7)

--- main.tf
+++ main.tf
@@ -1,23 +1,12 @@
-module "label" {
-  source      = "git::https://github.com/SweetOps/terraform-null-label.git?ref=tags/0.7.0"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  name        = var.name
-  stage       = var.stage
-  environment = var.environment
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
-}
-
 resource "google_project" "default" {
-  count               = var.enabled ? 1 : 0
-  name                = module.label.id
-  project_id          = module.label.id
-  labels              = module.label.gcp_labels
+  count = module.this.enabled ? 1 : 0
+
+  name                = module.this.id
+  project_id          = module.this.id
+  labels              = module.this.tags
   org_id              = var.org_id
   folder_id           = var.folder_id
-  skip_delete         = var.enable_deletion_protection
+  skip_delete         = var.skip_delete
   auto_create_network = var.auto_create_network
   billing_account     = var.billing_account_id
 }
